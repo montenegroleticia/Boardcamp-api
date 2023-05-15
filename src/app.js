b93743f8e1a75a10542c7a16ac40831b65c91ce9
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-05-15 11:34:58 -0300

    Update listen

--- src/app.js
+++ src/app.js
@@ -9,4 +9,6 @@ app.use(express.json());
 app.use(router);
 
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`Server running in port ${port}`));
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
